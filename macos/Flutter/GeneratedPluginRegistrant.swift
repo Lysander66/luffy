fb3550e151977d5021433826f164c4d0055f8835
Lysander <mgician3@gmail.com>
2023-01-31 22:10:20 +0800

    ss

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import flutter_webrtc
-import path_provider_macos
+import path_provider_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
